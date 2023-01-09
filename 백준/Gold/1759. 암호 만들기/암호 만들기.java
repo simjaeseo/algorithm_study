796672f512e1aa94e3556df757cbc2e69508efa7
simjaeseo <delpho@naver.com>
2023-01-09 11:34:01 +0900

    [Gold V] Title: 암호 만들기, Time: 84 ms, Memory: 11744 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,14 +1,13 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
     public static int L, C;
-    public static char[] list;
-    public static char[] code;
+    public static char[] inputCharacters;
+    public static char[] pickedCharacters;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
@@ -18,45 +17,44 @@ public class Main {
         L = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        list = new char[C];
-        code = new char[L];
+        inputCharacters = new char[C];
+        pickedCharacters = new char[L];
 
         st = new StringTokenizer(br.readLine());
 
-        for (int x = 0; x < C; x++) {
-            list[x] = st.nextToken().charAt(0);
+        for (int i = 0; i < C; i++) {
+            inputCharacters[i] = st.nextToken().charAt(0);
         }
 
+        Arrays.sort(inputCharacters);
 
-        // 정렬
-        Arrays.sort(list);
-
-        makeCode(0,0);
-
+        combination(0, 0);
     }
 
-    public static void makeCode(int x,int idx) {
+    public static void combination(int startIndex,int drawOfNumber) {
 
 
-        if (idx == L) {
+        if (drawOfNumber == L) {
+            // 최소 한개의 모음, 최소 2개의 자음인지 확인
             if (isValid()) {
-                System.out.println(code);
+                System.out.println(pickedCharacters);
             }
             return;
         }
 
+        // 아직 길이 L의 코드를 만들지 않았고 글자도 아직 남았다.
 
-        for (int i = x; i < C; i++) {
-            code[idx] = list[i];
-            makeCode(i+1, idx + 1);
+        for (int i = startIndex; i < C; i++) {
+            pickedCharacters[drawOfNumber] = inputCharacters[i];
+            combination(i+1, drawOfNumber + 1);
         }
     }
 
-    public static boolean isValid() {
+    private static boolean isValid() {
         int mo = 0;
         int ja = 0;
 
-        for (char x : code) {
+        for (char x : pickedCharacters) {
             if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
                 mo++;
             } else {
@@ -69,5 +67,4 @@ public class Main {
         }
         return false;
     }
-
 }
