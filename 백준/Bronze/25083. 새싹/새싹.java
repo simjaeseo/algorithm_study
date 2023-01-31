632503d5530b26dc64a531793aeab7f0f366cc24
simjaeseo <delpho@naver.com>
2023-01-31 09:02:16 +0900

    [Bronze V] Title: 새싹, Time: 76 ms, Memory: 11244 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7\n");
+        sb.append("r`-_   ,'  ,/\n");
+        sb.append(" \\. \". L_r'\n");
+        sb.append("   `~\\/\n");
+        sb.append("      |\n");
+        sb.append("      |\n");
+
+        System.out.println(sb);
+    }
+}
