4d3d7fcea57bc142ba612d520f2e0ea20388bf40
심재서 <delpho@naver.com>
2023-11-14 09:38:55 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+
+public class Main {
+    static double sumScore, sumGrade;
+    static long N, A, B, K, sum;
+    static char[] inputs;
+    static char[][] map = new char[5][15];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+    }
+
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+
+        N = Integer.parseInt(br.readLine());
+
+
+//        st = new StringTokenizer(br.readLine());
+
+        System.out.println(N*N*N);
+        System.out.println(3);
+
+    }
+
+
+}
