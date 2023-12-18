c6a7c0f42c698e52d86b3aed58c1917e04e429a7
심재서 <delpho@naver.com>
2023-12-18 13:11:47 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 792 ms, Memory: 58048 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    static int N;
+    static ArrayList<Integer> originList = new ArrayList<>();
+    static ArrayList<Integer> sortedList = new ArrayList<>();
+    static Set<String> set = new HashSet<>();
+    static Map<String, Boolean> map = new HashMap<>();
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        init();
+    }
+
+
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            String name = st.nextToken();
+            String command = st.nextToken();
+
+            if(command.equals("enter")){
+                set.add(name);
+            }else{
+                set.remove(name);
+            }
+        }
+
+
+        for(String name : set.stream().sorted(Collections.reverseOrder()).collect(Collectors.toList())){
+            sb.append(name).append('\n');
+        }
+
+        System.out.println(sb.substring(0, sb.length()-1));
+
+    }
+
+
+}
