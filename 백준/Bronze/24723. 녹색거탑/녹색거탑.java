352675d4c0b56d90e9972e7a40504cf1af17de0a
심재서 <delpho@naver.com>
2024-01-08 15:28:01 +0900

    [Bronze IV] Title: 녹색거탑, Time: 80 ms, Memory: 11564 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    private static int n, k;
+    private static Deque<Integer> qs = new ArrayDeque<>();
+    private static boolean[] isQueue;
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        print();
+    }
+
+    private static void solve() throws IOException {
+
+    }
+
+    private static void print() throws IOException {
+    }
+
+    private static void init() throws IOException {
+        n = Integer.parseInt(br.readLine());
+
+        System.out.println((int) Math.pow(2, n));
+    }
+}
