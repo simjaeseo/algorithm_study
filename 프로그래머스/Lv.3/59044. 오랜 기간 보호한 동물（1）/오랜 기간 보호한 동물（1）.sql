9f0c5e3a78577b556584e56e5d78ad781e822f01
심재서 <delpho@naver.com>
2024-01-15 13:05:26 +0900

    [level Lv.3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT AI.NAME, AI.DATETIME
+    FROM ANIMAL_INS AI LEFT OUTER JOIN ANIMAL_OUTS AO
+        USING (ANIMAL_ID)
+            WHERE AO.DATETIME IS NULL
+                ORDER BY AI.DATETIME
+                    LIMIT 3
