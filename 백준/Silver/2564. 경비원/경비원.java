d2183cb9c4d6a9687f4d31437347f029ef226a94
심재서 <delpho@naver.com>
2024-02-02 16:20:17 +0900

    [Silver I] Title: 경비원, Time: 72 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Silver/2564. 경비원/경비원.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int width = Integer.parseInt(st.nextToken());
+		int height = Integer.parseInt(st.nextToken());
+		
+		int storeCnt = Integer.parseInt(br.readLine());
+		int sum = 0;
+		int dongPos = 0;
+		int totalLength = (width+height)*2;
+		
+		int[] rectangle = new int[(width+height)*2];
+		
+		
+		
+		for (int i = 0; i <= storeCnt; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int direction = Integer.parseInt(st.nextToken());
+			int tmpPos = 0;
+			
+			switch (direction) {
+			case 1:
+				//북
+				tmpPos = Integer.parseInt(st.nextToken());
+				break;
+			case 4:
+				//동
+				tmpPos = width + Integer.parseInt(st.nextToken());
+				break;
+			case 2:
+				tmpPos = 2*width + height - Integer.parseInt(st.nextToken());
+				break;
+			case 3:
+				tmpPos = 2*width + 2*height - Integer.parseInt(st.nextToken());
+				break;
+			}
+			if(i == storeCnt)	dongPos = tmpPos;
+			rectangle[tmpPos] = 1;
+		}
+		
+		for (int i = 0; i < totalLength; i++) {
+			
+			if(dongPos + i == totalLength)	dongPos = -i;
+			if(rectangle[dongPos+i] == 1)	sum += Math.min(i, totalLength - i);
+			
+		}
+		
+		System.out.println(sum);
+	
+	}
+	
+	
+	
+
+}
