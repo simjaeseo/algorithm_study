27204b08deeac6daa39b08bedbf29b9604c14a98
심재서 <delpho@naver.com>
2024-02-14 16:42:50 +0900

    [Silver III] Title: 칸토어 집합, Time: 156 ms, Memory: 15300 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -1,43 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-//https://velog.io/@somyeong0623/%EB%B0%B1%EC%A4%80java-4779.-%EC%B9%B8%ED%86%A0%EC%96%B4-%EC%A7%91%ED%95%A9
 public class Main {
-    static int n;
-    static StringBuilder sb;
+    static int N, size, numbers[], inputs[];
+    static String s;
+    static char[] charArray;
+//    static ArrayList<Integer> l = new ArrayList<>();
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+//    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s;
-        while ((s = br.readLine()) != null) {
-            n = Integer.parseInt(s);
-            sb = new StringBuilder();
-            
-            int len = (int) Math.pow(3, n);
-            
-            for (int i = 0; i < len; i++) {
-                sb.append("-");
-            }
-
-            func(0, len);
-            
-            System.out.println(sb);
-        }
+        init();
     }
 
-    public static void func(int start, int size) {
-        if (size == 1) {
-            return;
+    private static void print() {
+        System.out.println(charArray);
+    }
+
+
+    private static void progress() {
+        size = (int) Math.pow(3, N);
+        charArray = new char[size];
+
+        for (int i = 0; i < size; i++) {
+            charArray[i] = '-';
         }
+
+        recursion(size, 0);
+    }
+
+    private static void recursion(int size, int start) {
+        if (size <= 1) return;
+
         int newSize = size / 3;
-        for (int i = start + newSize; i < start + 2 * newSize; i++) {
-            sb.setCharAt(i, ' ');
+
+        for (int i = start + newSize; i < start + newSize * 2; i++) {
+            charArray[i] = ' ';
         }
 
-        func(start, newSize);
-        func(start + 2 * newSize, newSize);
+        recursion(newSize, start); // 3등분한 문자열에서 첫번째 부분
+        recursion(newSize, start + newSize * 2); // 3등분한 문자열의 세번째 부분
     }
+
+    private static void init() throws IOException {
+        while ((s = br.readLine()) != null && !s.isEmpty()) {
+            N = Integer.parseInt(s);
+
+            progress();
+            print();
+
+        }
+
+    }
+
 
 }
