4a1abd77db140ffd72a891906a797b84fa2b7dd0
심재서 <delpho@naver.com>
2024-02-17 16:09:47 +0900

    [Silver III] Title: 파도반 수열, Time: 76 ms, Memory: 11420 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,34 +1,46 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
- 
-//https://st-lab.tistory.com/127
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-	static long[] seq = new long[101];
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		padovan();
-		
-		int T = Integer.parseInt(br.readLine());
-		for (int i = 0; i < T; i++) {
-			sb.append(seq[Integer.parseInt(br.readLine())]).append('\n');
-		}
-		
-		System.out.println(sb);
-	}
- 
-	public static void padovan() {
-		seq[1] = 1;
-		seq[2] = 1;
-		seq[3] = 1;
- 
-		for (int i = 4; i < 101; i++) {
-			seq[i] = seq[i - 2] + seq[i - 3];
-		}
-	}
- 
+    static long N,T, dp[];
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+    }
+
+    private static void process() {
+
+        for (int i = 4; i < 101; i++) {
+            dp[i] = dp[i-2] + dp[i-3];
+        }
+
+    }
+
+    public static void init() throws IOException {
+        T = Integer.parseInt(br.readLine());
+
+        dp = new long[101];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        process();
+
+        for (int i = 0; i < T; i++) {
+            N = Integer.parseInt(br.readLine());
+
+            sb.append(dp[(int)N]).append('\n');
+        }
+        System.out.print(sb);
+    }
 }
