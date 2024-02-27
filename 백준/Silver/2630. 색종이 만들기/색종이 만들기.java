6835a04fdcdd27e3ba3f43b39599e6a5ba19927e
심재서 <delpho@naver.com>
2024-02-27 12:22:56 +0900

    [Silver II] Title: 색종이 만들기, Time: 120 ms, Memory: 13156 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,66 +1,61 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
- 
-// https://st-lab.tistory.com/227
+import java.io.InputStreamReader;
+import java.util.*;
+
 public class Main {
-	
-	public static int white = 0;
-	public static int blue = 0;
-	public static int[][] board;
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		board = new int[N][N];
-		StringTokenizer st;
-		
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			
-			for(int j = 0; j < N; j++) {
-				board[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		partition(0, 0, N);
-		
-		System.out.println(white);
-		System.out.println(blue);
-		
-	}
-	
-	public static void partition(int row, int col, int size) {
-		if(colorCheck(row, col, size)) {
-			if(board[row][col] == 0) {
-				white++;
-			}
-			else {
-				blue++;
-			}
-			return;
-		}
-		
-		int newSize = size / 2;	
-		partition(row, col, newSize);						
-		partition(row, col + newSize, newSize);				
-		partition(row + newSize, col, newSize);			
-		partition(row + newSize, col + newSize, newSize);	
-	}
-	
-	
-	public static boolean colorCheck(int row, int col, int size) {
-		int color = board[row][col];	
-		
-		for(int i = row; i < row + size; i++) {
-			for(int j = col; j < col + size; j++) {
-				if(board[i][j] != color) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+    static int N, M, map[][], white, blue;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        init();
+
+        process();
+
+        System.out.println(white);
+        System.out.println(blue);
+    }
+
+    private static void process() {
+        recursion(0, 0, N);
+    }
+
+    private static void recursion(int startR, int startC, int size) {
+        boolean isAllBlue = true;
+        boolean isAllWhite = true;
+
+        for (int i = startR; i < startR + size; i++) {
+            for (int j = startC; j < startC + size; j++) {
+                if (map[i][j] == 0) isAllBlue = false;
+                else isAllWhite = false;
+            }
+        }
+
+        if(isAllBlue){
+            blue++;
+        }else if(isAllWhite){
+            white++;
+        }else{
+            recursion(startR, startC, size/2);
+            recursion(startR, startC + size/2,size/2);
+            recursion(startR+size/2, startC,size/2);
+            recursion(startR + size/2, startC + size/2,size/2);
+        }
+    }
+
+    public static void init() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
 }
