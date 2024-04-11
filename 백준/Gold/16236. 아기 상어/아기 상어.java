5223dcc367b09c9508f8fc03202195033cc0c17e
심재서 <delpho@naver.com>
2024-04-11 11:31:36 +0900

    [Gold III] Title: 아기 상어, Time: 88 ms, Memory: 12088 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -2,10 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, maxSecond;
-    static int[][] map;
-    static boolean[][] isVisited;
+    static int N, map[][], resultSecond;
     static Shark shark;
+    static boolean isVisited[][];
     static int[] dr = new int[]{0, 0, 1, -1};
     static int[] dc = new int[]{1, -1, 0, 0};
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,23 +16,22 @@ public class Main {
 
         process();
 
-        System.out.println(maxSecond);
+        System.out.println(resultSecond);
     }
 
     private static void process() {
-        bfsAndEatFish();
+        bfs();
     }
 
-    private static void bfsAndEatFish() {
+    private static void bfs() {
 
-        Queue<int[]> q = new LinkedList<>();
+        Queue<int[]> q = new ArrayDeque<>();
         isVisited = new boolean[N][N];
+        int distance = 0;
+        PriorityQueue<Fish> eatableFishPQ = new PriorityQueue<>();
 
         q.add(new int[]{shark.r, shark.c});
         isVisited[shark.r][shark.c] = true;
-
-        PriorityQueue<Fish> pq = new PriorityQueue<>();
-        int distance = 0;
 
         while(!q.isEmpty()){
             int qSize = q.size();
@@ -45,44 +43,43 @@ public class Main {
                     int mr = sharkPosition[0] + dr[j];
                     int mc = sharkPosition[1] + dc[j];
 
-                    if(mr < 0 || mc < 0 || mr >= N || mc >= N || isVisited[mr][mc]) continue;
+                    if(mr<0 || mc < 0 || mr>= N || mc>= N || isVisited[mr][mc]) continue;
 
                     if(map[mr][mc] != 0 && map[mr][mc] < shark.size){
-                        pq.add(new Fish(mr,mc,map[mr][mc]));
+                        eatableFishPQ.add(new Fish(mr,mc,map[mr][mc]));
                     }
                     if(map[mr][mc] <= shark.size){
                         q.add(new int[]{mr,mc});
                         isVisited[mr][mc] = true;
                     }
                 }
-            }
 
+            }
             distance++;
 
-            if(!pq.isEmpty()){
-                Fish fish = pq.poll();
-                shark.countEattingFish++;
-                map[fish.r][fish.c] = 0; //물고기 먹었으니 그 자리는 0으로 만들기
+            if(!eatableFishPQ.isEmpty()){
+                Fish eatableFish = eatableFishPQ.poll();
 
-                if(shark.countEattingFish == shark.size){
+                map[eatableFish.r][eatableFish.c] = 0;
+                resultSecond += distance;
+                distance = 0;
+                shark.countEattingFish++;
+                q.clear();
+                eatableFishPQ.clear();
+                isVisited = new boolean[N][N];
+
+                if(shark.size == shark.countEattingFish){
                     shark.size++;
                     shark.countEattingFish = 0;
                 }
 
-                maxSecond += distance; // 물고기 위치까지 간 시간 더해주기
-                distance = 0;
-                pq.clear();
-                q.clear();
-                isVisited = new boolean[N][N];
-                q.add(new int []{fish.r, fish.c});
-                isVisited[fish.r][fish.c] = true;
+                q.add(new int[]{eatableFish.r, eatableFish.c, eatableFish.size});
             }
         }
     }
 
 
     private static void init() throws IOException {
-
         N = Integer.parseInt(br.readLine());
 
         map = new int[N][N];
@@ -93,47 +90,22 @@ public class Main {
             for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
 
-                if (map[i][j] == 9) {
-                    shark = new Shark(i, j, 2, 0);
+                if(map[i][j] == 9){
+                    shark = new Shark(i,j,2,0);
                     map[i][j] = 0;
                 }
             }
         }
     }
 
-    public static class Fish implements Comparable<Fish> {
-        int r;
-        int c;
-        int size;
 
-        public Fish(int r, int c, int size) {
-            this.r = r;
-            this.c = c;
-            this.size = size;
-        }
-
-
-        @Override
-        public int compareTo(Fish o) {
-            int res = 0;
-
-            if (this.r == o.r) {
-                res = this.c - o.c;
-            } else {
-                res = this.r - o.r;
-            }
-            return res;
-        }
-
-    }
-
-    public static class Shark {
+    public static class Shark{
         int r;
         int c;
         int size;
         int countEattingFish;
 
-        public Shark(int r, int c, int size, int countEattingFish) {
+        public Shark(int r, int c, int size, int countEattingFish){
             this.r = r;
             this.c = c;
             this.size = size;
@@ -141,4 +113,24 @@ public class Main {
         }
     }
 
+    private static class Fish implements Comparable<Fish>{
+        int r;
+        int c;
+        int size;
+
+        public Fish(int r, int c, int size){
+            this.r = r;
+            this.c = c;
+            this.size = size;
+        }
+
+        @Override
+        public int compareTo(Fish o){
+            if(this.r == o.r){
+                return this.c - o.c;
+            }else{
+                return this.r - o.r;
+            }
+        }
+    }
 }
