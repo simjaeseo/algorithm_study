31472b5bfa192c16439d2b6f26e46a29d98a514a
심재서 <delpho@naver.com>
2024-05-25 21:02:14 +0900

    [Bronze V] Title: 웰컴, Time: 72 ms, Memory: 11292 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int dp[][], previousDPNumber;
+    static String A, B;
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static StringBuilder sb2 = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        init();
+//        process();
+    }
+
+    private static void process() {
+
+
+
+    }
+
+    private static void init() throws IOException {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+
+    }
+
+}
