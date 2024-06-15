fef83539a1937e350601065f557409dc0d74376e
심재서 <delpho@naver.com>
2024-06-15 19:33:10 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 516 ms, Memory: 126004 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 
+//if else 테스트
 public class Main {
     static int N, M, result;
     static int[][] map;
@@ -21,7 +22,7 @@ public class Main {
 
     private static void process() {
 
-        System.out.println(bfs());
+        System.out.println(bfs2());
     }
 
     private static int bfs() {
@@ -56,6 +57,38 @@ public class Main {
 
         return -1;
 
+    }
+
+
+    private static int bfs2(){
+        Queue<Position> q = new LinkedList<>();
+        q.add(new Position(0,0,0));
+        isVisited[0][0][0] = 1;
+
+        while(!q.isEmpty()){
+            Position position = q.poll();
+
+            if(position.r == N-1 && position.c == M-1)  return isVisited[position.breakingFlag][position.r][position.c];
+
+            for (int i = 0; i < 4; i++) {
+                int mr = position.r + dr[i];
+                int mc = position.c + dc[i];
+                int nextBreakingFlag = position.breakingFlag;
+
+                if(mr<0||mc<0||mr>=N||mc>=M)    continue;
+                if(isVisited[position.breakingFlag][mr][mc] > 0)    continue;
+
+                if(map[mr][mc] == 0){
+                    isVisited[nextBreakingFlag][mr][mc] = isVisited[position.breakingFlag][position.r][position.c] + 1;
+                    q.add(new Position(mr,mc,nextBreakingFlag));
+                }else if(map[mr][mc] == 1 && position.breakingFlag == 0){
+                    nextBreakingFlag = 1;
+                    isVisited[nextBreakingFlag][mr][mc] = isVisited[position.breakingFlag][position.r][position.c] + 1;
+                    q.add(new Position(mr,mc,nextBreakingFlag));
+                }
+            }
+        }
+        return -1;
     }
 
     private static void init() throws IOException {
