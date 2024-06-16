ceb821b47bfae61fbc9323c17eed1aa6684e13a5
심재서 <delpho@naver.com>
2024-06-16 21:31:18 +0900

    [Bronze V] Title: Rook, Time: 76 ms, Memory: 11400 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+    }
+}
