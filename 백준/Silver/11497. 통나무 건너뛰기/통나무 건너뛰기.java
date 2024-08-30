bf8a5a91da7d57c20b1fb53717eee21118efe65b
심재서 <delpho@naver.com>
2024-08-30 18:30:15 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 412 ms, Memory: 49964 KB -BaekjoonHub

--- 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.java
@@ -18,59 +18,26 @@ class Main {
 
     private static void solve() {
 
-        /*
-
-        int[]의 각각의 차가 가장 작은 경우를 찾아야 한다.
-
-        2 4 5 7 9
-
-        2 9 7 4 5
-
-        2 5 9 7 4
-
-        13 10 12 11 10 11 12
-
-        10 10 11 11 12 12 13
-
-        10 11 12 13 12 11 10
-
-        1 5 9 29 15 2
-
-        1 2 5 9 15 29
-
-        1 5 15 29 9 2
-
-        4 10 14 20 7 1
-
-
-
-         */
-
         Arrays.sort(trees);
-        int index = 0;
 
-        for (int i = 0; i < N; i+=2) {
-            answer[index++] = trees[i];
-        }
+        int left = 0;
+        int right = N-1;
 
-        int i = N - 1;
-        if((N-1)%2 == 0) i--;
-
-        for (; i >= 0; i-=2) {
-            answer[index++] = trees[i];
+        for (int i = 0; i < N; i++) {
+            if(i % 2 == 0){
+                answer[left++] = trees[i];
+            }else{
+                answer[right--] = trees[i];
+            }
         }
 
         int max = 0;
-
         for (int j = 0; j < N-1; j++) {
             max = Math.max(Math.abs(answer[j] - answer[j+1]), max);
         }
 
         max = Math.max(Math.abs(answer[0] - answer[N-1]), max);
 
-
-//        System.out.println(Arrays.toString(trees));
-//        System.out.println(Arrays.toString(answer));
         System.out.println(max);
     }
 
