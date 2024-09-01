dee333b54ab51c80f0a2e247cc371bb6d028475c
심재서 <delpho@naver.com>
2024-09-01 19:38:23 +0900

    [Gold V] Title: 배, Time: 212 ms, Memory: 15220 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -4,8 +4,9 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 class Main {
-    static int N, M, crain[], boxes[], numberOfBoxesRemaining;
-    static boolean isSelectedBoxes[];
+    static int N, M;
+    static Integer cranes[];
+    static List<Integer> boxes = new ArrayList<>();
 
     static StringBuilder sb = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,75 +20,53 @@ class Main {
 
 
     private static void solve() {
-        int startIndex = 0;
+
+        // 크레인과 박스를 내림차순으로 정렬
+        Arrays.sort(cranes, Collections.reverseOrder());
+        Collections.sort(boxes, Collections.reverseOrder());
+
+        // 가장 무거운 박스를 들 수 있는 크레인이 없으면 불가능
+        if (boxes.get(0) > cranes[0]) {
+            System.out.println(-1);
+            return;
+        }
+
         int time = 0;
-        int boxIndex = 0;
 
-        Arrays.sort(crain);
-        Arrays.sort(boxes);
+        // 모든 박스를 옮길 때까지 반복
+        while (!boxes.isEmpty()) {
+            int boxIndex = 0, craneIndex = 0;
 
-        while (true) {
-            if(numberOfBoxesRemaining == 0) {
-                System.out.println(time);
-                break;
-            }
-
-            if(startIndex >= N) {
-                System.out.println(-1);
-                break;
-            }
-
-            for (int i = startIndex; i < crain.length; i++) {
-                if(numberOfBoxesRemaining == 0) break;
-                if(startIndex >= N) break;
-
-                boolean isCarry = false;
-                for (int j = M-1; j >= 0; j--) {
-
-                    // 크레인이 들 수 있는 무게이면서, 아직 안 옮긴 박스라면?
-                    if(crain[i] >= boxes[j] && !isSelectedBoxes[j]){
-                        isSelectedBoxes[j] = true;
-                        numberOfBoxesRemaining--;
-                        isCarry = true;
-                        break;
-                    }
-
+            while(craneIndex < N){
+                if(boxIndex == boxes.size())    break;
+                else if(cranes[craneIndex] >= boxes.get(boxIndex)){
+                    boxes.remove(boxIndex);
+                    craneIndex++;
+                }else{
+                    boxIndex++;
                 }
-                // i번째 크레인이 들 수 있는 박스가 없다면? 다음 시간에서는 i번째가 들 수 있는 박스가 아예 없다는 의미이니, 시작 인덱스를 +1 해줘야 최적화됨
-                if(!isCarry) startIndex++;
-
             }
-
             time++;
         }
+
+        System.out.println(time);
     }
 
-/*
-3
-5 6 10
-11
-6 6 6 6 6 8 8 8 9 9 9
-
- */
     private static void init() throws IOException {
 
         N = Integer.parseInt(br.readLine());
-        crain = new int[N];
+        cranes = new Integer[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            crain[i] = Integer.parseInt(st.nextToken());
+            cranes[i] = Integer.parseInt(st.nextToken());
         }
 
         M = Integer.parseInt(br.readLine());
-        boxes = new int[M];
-        isSelectedBoxes = new boolean[M];
-        numberOfBoxesRemaining = M;
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            boxes[i] = Integer.parseInt(st.nextToken());
+            boxes.add(Integer.parseInt(st.nextToken()));
         }
     }
-
 }
