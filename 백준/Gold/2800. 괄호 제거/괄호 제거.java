b54e2e88c6497cc115c17fff64b1fec731100722
delphoo <delpho@naver.com>
2024-09-20 17:11:01 +0900

    [Gold IV] Title: 괄호 제거, Time: 192 ms, Memory: 17336 KB -BaekjoonHub

---
+++ 백준/Gold/2800. 괄호 제거/괄호 제거.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    static List<int[]> brackets = new ArrayList<>();
+    static Set<String> result = new TreeSet<>();
+    static boolean isSelected[];
+
+    static StringBuilder origin = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+    }
+
+    private static void solve() {
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < origin.length(); i++) {
+            if(origin.charAt(i) == '(') {
+                stack.push(i);
+            } else if (origin.charAt(i) == ')') {
+                int left = stack.pop();
+                int right = i;
+                brackets.add(new int[]{left, right});
+            }
+        }
+
+        isSelected = new boolean[brackets.size()];
+
+        subset(0);
+
+        result.remove(origin.toString()); // 원본 수식은 제외
+
+        for (String res : result) {
+            System.out.println(res);
+        }
+    }
+
+    private static void subset(int count) {
+        if(count == brackets.size()){
+            boolean zeroSet = false;
+
+            StringBuilder sb = new StringBuilder(origin);
+
+            for (int i = 0; i < brackets.size(); i++) {
+                if(isSelected[i]){
+                    sb.setCharAt(brackets.get(i)[0], ' ');
+                    sb.setCharAt(brackets.get(i)[1], ' ');
+                    zeroSet = true;
+                }
+            }
+            if(!zeroSet) return;
+
+            String res = sb.toString().replaceAll(" ", "");
+            result.add(res);
+            return;
+        }
+
+        isSelected[count] = true;
+        subset(count+1);
+
+        isSelected[count] = false;
+        subset(count+1);
+    }
+
+    private static void init() throws IOException {
+        String input = br.readLine();
+        origin = new StringBuilder(input);
+    }
+}
