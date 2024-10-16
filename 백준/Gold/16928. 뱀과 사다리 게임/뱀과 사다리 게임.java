462ec6a634f73685ffeee2e2b124f1d465ea0294
simjaeseo <delpho@naver.com>
2024-10-16 16:17:01 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 11684 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -6,10 +6,8 @@ import java.util.*;
 class Main {
     static int map[], N, M;
     static boolean isVisited[];
-    static Map<Integer, Integer> ladders = new HashMap<>();
-    static Map<Integer, Integer> snakes = new HashMap<>();
+    static Map<Integer, Integer> laddersAndSnakes = new HashMap<>();
 
-    static StringBuilder sb = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
@@ -21,69 +19,39 @@ class Main {
 
 
     private static void solve() {
-        System.out.println(bfs(1));
-
-        /*
-        1. 문제 이해
-
-        2. 요구 사항 분해
-
-        3. 핵심 로직 설계
-
-            "다른 관점에서 이 문제를 바라볼 수 있을까?"
-
-        4. 조건 및 예외 처리 고려
-
-        5. 코드 구조 잡기
-
-        6. 구현 시작
-
-
-         */
-
+        bfs();
 
     }
 
-    private static int bfs(int start) {
-        Queue<int[]> q = new ArrayDeque<>();
-        isVisited[start] = true;
-        q.add(new int[]{start, 0});
+    private static void bfs() {
+        Queue<Integer> q = new ArrayDeque<>();
+        isVisited[1] = true;
+        q.add(1);
 
-        while(!q.isEmpty()){
-            int[] position = q.poll();
+        while (!q.isEmpty()) {
+            int current = q.poll();
 
-            for (int i = 1; i <= 6; i++) {
-                int nextPosition = position[0] + i;
-                int nextCount = position[1] + 1;
+            for (int dice = 1; dice <= 6; dice++) {
+                int next = current + dice;
 
-                if(nextPosition > 100) continue;
-                if(nextPosition == 100) return nextCount;
-                if(isVisited[nextPosition]) continue;
-
-                if(ladders.containsKey(nextPosition)){
-                    if(isVisited[ladders.get(nextPosition)]) continue;
-
-                    q.add(new int[] {ladders.get(nextPosition), nextCount});
-                    isVisited[ladders.get(nextPosition)] = true;
-                }else if(snakes.containsKey(nextPosition)){
-                    if(isVisited[snakes.get(nextPosition)]) continue;
-
-                    q.add(new int[] {snakes.get(nextPosition), nextCount});
-                    isVisited[snakes.get(nextPosition)] = true;
-                }else{
-                    q.add(new int[] {nextPosition, nextCount});
-                    isVisited[nextPosition] = true;
+                if (next == 100) {
+                    System.out.print(map[current]+1);
+                    return;
                 }
 
+                if (next > 100) continue;
 
+                if (laddersAndSnakes.containsKey(next)) {
+                    next = laddersAndSnakes.get(next);
+                }
+
+                if (isVisited[next]) continue;
+
+                q.add(next);
+                isVisited[next] = true;
+                map[next] = map[current] + 1;
             }
-
-
         }
-
-        return 0;
-
-
     }
 
 
@@ -96,26 +64,13 @@ class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N + M; i++) {
             st = new StringTokenizer(br.readLine());
 
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            ladders.put(start, end);
+            laddersAndSnakes.put(start, end);
         }
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            snakes.put(start, end);
-        }
-
-
-
     }
-
 }
