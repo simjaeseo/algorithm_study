b97a7a1835ca23a9c9a8f4f2668d970aa2cf866f
simjaeseo <delpho@naver.com>
2024-10-18 19:53:54 +0900

    [Silver I] Title: 포도주 시식, Time: 88 ms, Memory: 13452 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,51 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
-public class Main {
-    static int N, wine[];
+class Main {
+    static int N, wines[];
     static Integer dp[];
 
+    static StringBuilder sb = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
         init();
-        process();
+
+        solve();
     }
 
 
-    private static void process() {
-
+    private static void solve() {
+        dp = new Integer[N+1];
         dp[0] = 0;
-        dp[1] = wine[1];
+        dp[1] = wines[1];
         
-        if(N>=2){
-            dp[2] = wine[1] + wine[2];
-        }
+        if(N >= 2) dp[2] = wines[1] + wines[2];
 
-        for (int i = 3; i <= N; i++) {
-            dp[i] = Math.max(Math.max(dp[i-1], dp[i-2]+ wine[i]), dp[i-3] + wine[i-1] + wine[i]);
-        }
-
-        System.out.println(dp[N]);
+        System.out.println(dp(N));
 
     }
 
+    private static int dp(int n) {
+
+        if(dp[n] == null){
+            dp[n] = Math.max(Math.max(dp(n-3) + wines[n-1]+ wines[n], dp(n-2)+ wines[n]), dp(n-1)) ;
+        }
+
+        return dp[n];
+    }
 
     private static void init() throws IOException {
-
         N = Integer.parseInt(br.readLine());
-
-        // dp[n] = n번째까지 먹을 수 있는 최대 와인의 양
-        // dp는 하위 문제들의 결과를 어떻게 결합하여 상위 문제의 결과를 만들 수 있을지 생각
-        // n == 3 : 1,5,3
-        // n == 4 : 1,5,3,7
-        // n == 5 : 1,5,3,7,2
-        dp = new Integer[N + 1];
-        wine = new int[N + 1];
+        wines = new int[N+1];
 
         for (int i = 1; i <= N; i++) {
-            wine[i] = Integer.parseInt(br.readLine());
+            wines[i] = Integer.parseInt(br.readLine());
         }
     }
 }
