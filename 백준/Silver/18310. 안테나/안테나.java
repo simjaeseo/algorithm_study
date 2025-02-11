c02b41b30d6c275ae8cb26bd57968098ca2e3b62
simjaeseo <delpho@naver.com>
2025-02-11 18:06:30 +0900

    [Silver III] Title: 안테나, Time: 396 ms, Memory: 43560 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.java
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -4,8 +4,8 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 class Main {
-    static int N, positions[];
-    static Set<Integer> set = new HashSet<>();
+    static int N, houses[];
+
     static StringBuilder sb = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
@@ -19,21 +19,33 @@ class Main {
 
     private static void solve() {
 
-        Arrays.sort(positions);
 
-        System.out.println(positions[(N-1)/2]);
+        Arrays.sort(houses);
+
+        if(houses.length % 2 != 0){
+            System.out.println(houses[houses.length/2]);
+        }else{
+            System.out.println(houses[houses.length/2 - 1]);
+        }
+
+        /*
+        안테나로부터 모든 집까지의 거리의 총 합이 최소.
+
+        - 집 위치를 정렬해야함.
+        - 집 위치 중 가장 가운데에 설치해야함.
+
+         */
+
     }
-
 
 
     private static void init() throws IOException {
         N = Integer.parseInt(br.readLine());
-        positions = new int[N];
+        houses = new int[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            positions[i] = (Integer.parseInt(st.nextToken()));
+            houses[i] = Integer.parseInt(st.nextToken());
         }
-
     }
 }
