d121f2e131ea3eaab4ea1a23597197358a4ab48e
simjaeseo <delpho@naver.com>
2025-02-26 13:19:10 +0900

    [Gold V] Title: 센서, Time: 128 ms, Memory: 14824 KB -BaekjoonHub

--- 백준/Gold/2212. 센서/센서.java
+++ 백준/Gold/2212. 센서/센서.java
@@ -4,10 +4,7 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 class Main {
-    static int N, K, sensors[];
-    static Set<Integer> set = new HashSet<>();
-    static List<Integer> distances = new ArrayList<>();
-    static List<Integer> cutPoint = new ArrayList<>();
+    static int N, K, position[], distance[];
 
     static StringBuilder sb = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,96 +18,33 @@ class Main {
 
 
     private static void solve() {
-        // 센서들의 간격들을 List에 먼저 저장.
-        for (int i = 0; i < sensors.length - 1; i++) {
-            distances.add(sensors[i+1] - sensors[i]);
+
+        Arrays.sort(position);
+
+        for (int i = 0; i < N-1; i++) {
+            distance[i] = position[i+1] - position[i];
+        }
+        Arrays.sort(distance);
+
+        int total = 0;
+        for (int i = 0; i < N - K; i++) {
+            total += distance[i];
         }
 
-        // 내림차순으로 정렬
-        Collections.sort(distances, Collections.reverseOrder());
-
-        // N-1개를 cutPoint 리스트에 저장
-        for (int i = 0; i < Math.min(sensors.length - 1, K - 1); i++) {
-            cutPoint.add(distances.get(i));
-        }
-
-        int result = 0;
-        for (int i = 0; i < sensors.length-1; i++) {
-            int distance = sensors[i+1] - sensors[i];
-
-            if(cutPoint.contains(distance)){
-                cutPoint.remove(new Integer(distance));
-                continue;
-            }
-
-            result += distance;
-        }
-
-        System.out.println(result);
-
+        System.out.println(total);
 
     }
-
 
     private static void init() throws IOException {
         N = Integer.parseInt(br.readLine());
         K = Integer.parseInt(br.readLine());
 
+        position = new int[N];
+        distance = new int[N-1];
+
         st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < N; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
+            position[i] = Integer.parseInt(st.nextToken());
         }
-
-        sensors = new int[set.size()];
-
-        int i = 0;
-        for(int number : set){
-            sensors[i++] = number;
-        }
-        Arrays.sort(sensors);
-
     }
-
 }
-/*
-6
-2
-1 6 9 3 6 7
-1 3 6 6 7 9
-1 ~ 9
-
-
-10
-5
-3 6 7 8 10 12 14 15 18 20
- 3 1 1 2  2  2  1  3  2
-
-3 3 2 2 2 2 1 1 1
-
-3 3 2 2
-
-3/ 6 7 8/ 10 12/ 14 15/ 18/ 20
-
-
-10개를 5개가 커버
-집중국 1개당 최대 2개까지 커버해야함
-
-
-0   2     2     1    2
-1 3 2 2 2
-
- 1개당 4개 이하
-
-
-17 -> 5개가 커버
-3.4개
-
-3 ~ 20
-
-17
-
-
-
-
- */
